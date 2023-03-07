dd459abf854a9adf302be762e06f532ad1bfa092
Tom Johansson <Tom.Johansson@yh.nackademin.se>
2023-03-07 10:05:11 +0100

    Changed route patch
    it now patches to database

--- src/controllers/adminControllers/patch.js
+++ src/controllers/adminControllers/patch.js
@@ -1,7 +1,35 @@
-const { countries } = require('../../database/land')
+/**
+ * update a country
+ * Question: what can be solved in the database, and what should be solved in the code?
+ * Changing the name of a country is not possible in the database, so it should be solved in the code
+ * Should we query the database to check if the country exists? Or should we just try to update it and catch the error?
+ */
+
 const joi = require('joi')
 
+
+//database connection, TO DO: move to separate file, maybe use mysql2/promise
+const mysql = require('mysql2'); 
+const {config} = require('../../database/config');
+const pool = mysql.createPool(config);
+
+//creating a array with names so we can check if the country exists, Do we need this or can we just try to update and catch the error?
+// let data = []
+// pool.query("SELECT * FROM Countries", function(err, rows, fields) {
+//   // Connection is automatically released when query resolves'
+//   if (err) {
+//       console.log("err",err)
+//   }
+
+//   //push data to array
+//   rows.forEach(row => {
+//     data.push(row)
+//   }
+//   )
+// })
+
 exports.patchFunction = function patchFunction (req, res) {
+
     const schema = joi.object({
         name: joi.string().required(),
         capital: joi.string().required(),
@@ -9,19 +37,44 @@ exports.patchFunction = function patchFunction (req, res) {
     })
 
     const validate = schema.validate(req.body);
-
     if (validate.error) {
         return res.status(400).json(validate.error.details[0].message)
     }
-    const { name } = req.body
 
-    const chosenCountry = countries.findIndex(country => country.name === name)
 
-    if (chosenCountry === -1) {
-        return res.status(404).send('This country doesnt exist 🤷 ')
+    //check if country exists  
+    // const countryIndex = data.findIndex(chosenCountry => chosenCountry.Name === req.body.name)  //to do ->Name is case sensitive
+
+    // if (countryIndex === -1) {
+    //     return res.status(404).send('This country does not exist in our database')
+    // }
+
+
+    pool.execute('UPDATE Countries SET Capital = ?, Population = ? WHERE Name = ?', [req.body.capital, req.body.population, req.body.name], (err, result) => {
+        if (err) {
+            console.log(err)
+            if (err.code === 'ER_NO_REFERENCED_ROW_2') {
+                res.status(400).send('Country does not exist')
+                return;
+            }
+            res.status(500).send('Something went wrong')
+            return;
+        }
+        if (result.affectedRows === 0) {
+            res.status(404).send('Country does not exist')
+            return;
+        }
+        if (result.changedRows === 0) {
+            res.status(400).send('Country already has this capital or population')
+            return;
+        }
+        if (result.changedRows === 1) {
+            res.status(200).send('Country updated')
+            return;
+        }
+    
+        res.status(200).send(result)
     }
-
-    countries[chosenCountry] = req.body
-    res.status(200).send(`You have changed info for the country: ${name} 😃`)
+    )
     
 }
