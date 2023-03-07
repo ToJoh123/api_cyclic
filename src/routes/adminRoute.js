005947ff669a1c011492d28111d18f195ba2ef6f
Tom Johansson <Tom.Johansson@yh.nackademin.se>
2023-03-07 11:11:03 +0100

    added authcookies to route
    now auth cookies is required to make changes

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -3,12 +3,12 @@ const { deleteFunction } = require('../controllers/adminControllers/delete');
 // const { getFunction } = require('../controllers/adminControllers/get');
 const { patchFunction } = require('../controllers/adminControllers/patch');
 const { postFunction } = require('../controllers/adminControllers/post');
-// const { authCookie } = require('../middleware/authCookie');
+const { authCookie } = require('../middleware/authCookie');
 const adminRoute = express.Router();
 
 // adminRoute.get('/', getFunction );
-adminRoute.post('/', postFunction );
-adminRoute.patch('/', patchFunction );
-adminRoute.delete('/', deleteFunction );
+adminRoute.post('/',authCookie, postFunction );
+adminRoute.patch('/',authCookie, patchFunction );
+adminRoute.delete('/', authCookie, deleteFunction );
 
 exports.adminRoute = adminRoute;
